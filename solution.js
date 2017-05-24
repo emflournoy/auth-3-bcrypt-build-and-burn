effce74d46561cdbc8a5a29bc310a5d60af39b21
Elizabeth Flournoy <bflournoy@gmail.com>
2017-05-24 14:03:17 -0600

    completed assignment

--- solution.js
+++ solution.js
@@ -1,9 +1,12 @@
-function hash(plaintext) {
+const bcrypt = require('bcrypt');
+const saltRounds = 8;
 
+function hash(plaintext) {
+  return bcrypt.hashSync(plaintext,8);
 }
 
 function compare(plaintext, hash) {
-
+  return bcrypt.compareSync(plaintext, hash)
 }
 
 module.exports = {
